32f19e0bb73bfa000450a9f1ca57a9086f59942d
root <root@ip-172-31-13-206.ap-south-1.compute.internal>
2025-08-05 11:29:08 +0000

    New code Added

--- src/main/java/grp1/App.java
+++ src/main/java/grp1/App.java
@@ -1,9 +1,17 @@
-package grp1;
+import javax.swing.*;
 
-public class App {
+public class HelloApp {
+    public static void main(String[] args) {
+        // Create a window
+        JFrame frame = new JFrame("Simple App");
+        frame.setSize(300, 200);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-  public static void main(String[] args) {
-    System.out.println("Hello World!");
-  }
+        // Add a button
+        JButton button = new JButton("Click Me!");
+        button.addActionListener(e -> JOptionPane.showMessageDialog(frame, "Hello, World!"));
 
+        frame.getContentPane().add(button);
+        frame.setVisible(true);
+    }
 }
